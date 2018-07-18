401207b104a7628ffe3adbe40a32036f27ba7d10
谢贤飞 <137032979@qq.com>
2018-07-18 09:15:38 +0800

    form

---
+++ duzuimoye-xxf_js_fullstack/vue/ele-form-code/src/components/ElFormItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="el-form-item"> 
+        <label class="el-form-item__label" v-if="label || $slots.label">
+            <slot name="label">{{label}}</slot>
+        </label>
+        <div class="el-form-item__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import emitter from '@/mixins/emitter'
+export default {
+    props: {
+        label:String,
+        prop:String
+    },
+    mixins:[emitter],
+    mounted(){
+        // filed 生成一下
+        this.dispatch('ElForm','el.form.addField',[this]);
+    },
+    computed: {
+        form(){
+            let parent = this.$parent;
+            let parentName = parent.$options.componentName;
+            while(parentName !== 'ElForm') {
+                parent = parent.$parent;
+                parentName = parent.$options.componentName;
+            }
+            return parent;
+        }
+    },
+    methods: {
+        validate (trigger,callback) {
+            const rules = this.form.rules;
+            console.log('aaa');
+            // console.log(rules);
+            const item_rule = rules[this.prop];
+            console.log(item_rule);
+            // app.vue rules Eleform props form.
+            // 当前item  rules [name] 对应的rule是什么？
+            // el-input 值是什么
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
